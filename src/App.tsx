45e8dd8a417ea8c3c565210acd4e8ce8ed67accb
oh-ji-yeon <o.delayed0106@gmail.com>
2024-06-20 10:48:30 +0900

    common component - input, alert, flexBox

--- src/App.tsx
+++ src/App.tsx
@@ -2,8 +2,14 @@ import './App.css'
 
 import Text from '@shared/Text'
 import Button from '@shared/Button'
+import Input from '@shared/Input'
+import TextField from '@shared/TextField'
+import Alert from '@shared/Alert'
+import { useAlertContext } from './contexts/AlertContext'
 
 function App() {
+  const { open } = useAlertContext()
+
   return (
     <div>
       <Text typography="t1" display="block" color="red">
@@ -31,6 +37,37 @@ function App() {
       <Button full={true}>클릭해주세요 ;)</Button>
       <Button full={true} disabled={true}>
         클릭해주세요 ;)
+      </Button>
+
+      <div style={{ height: 10, width: '100%', background: '#efefef' }} />
+
+      <Input placeholder="로그인" aria-invalid={false} />
+      <Input aria-invalid={true} />
+
+      <TextField label="아이디" />
+      <TextField label="패스워드" hasError={true} />
+
+      <div style={{ height: 10, width: '100%', background: '#efefef' }} />
+
+      {/* <Alert
+        open={true}
+        description="안녕하세요!"
+        title="알럿이 떴습니다~"
+        onButtonClick={() => {}}
+      /> */}
+
+      <Button
+        onClick={() => {
+          open({
+            title: '카드신청완료 ;)',
+            description: '내역페이지에서 확인해주세요.',
+            onButtonClick: () => {
+              //
+            },
+          })
+        }}
+      >
+        Alert오픈
       </Button>
     </div>
   )
---
+++ src/components/shared/TextField.tsx
@@ -0,0 +1,72 @@
+import {
+  FocusEventHandler,
+  forwardRef,
+  InputHTMLAttributes,
+  useState,
+} from 'react'
+import Input from './Input'
+import Text from './Text'
+
+interface TextFieldProps extends InputHTMLAttributes<HTMLInputElement> {
+  label?: React.ReactNode
+  hasError?: boolean
+  helpMessage?: React.ReactNode
+}
+
+const TextField = forwardRef<HTMLInputElement, TextFieldProps>(
+  function TextField(
+    { label, hasError, helpMessage, onFocus, onBlur, ...props },
+    ref,
+  ) {
+    // 포커스 처리
+    const [focused, setFocused] = useState(false)
+
+    const labelColor = hasError ? 'red' : focused ? 'blue' : undefined
+
+    const hadleFocus: FocusEventHandler<HTMLInputElement> = (event) => {
+      setFocused(true)
+      onFocus?.(event)
+    }
+
+    const hadleBlur: FocusEventHandler<HTMLInputElement> = (event) => {
+      setFocused(false)
+      onBlur?.(event)
+    }
+
+    return (
+      <div>
+        {label ? (
+          <Text
+            typography="t7"
+            color={labelColor}
+            display="inline-block"
+            style={{ marginBottom: 6 }}
+          >
+            {label}
+          </Text>
+        ) : null}
+
+        <Input
+          ref={ref}
+          aria-invalid={hasError}
+          onFocus={hadleFocus}
+          onBlur={hadleBlur}
+          {...props}
+        />
+
+        {helpMessage ? (
+          <Text
+            typography="t7"
+            color={labelColor}
+            display="inline-block"
+            style={{ marginTop: 6, fontSize: 12 }}
+          >
+            {helpMessage}
+          </Text>
+        ) : null}
+      </div>
+    )
+  },
+)
+
+export default TextField
